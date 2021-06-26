e5ce818987016547ab7c59fb7adcdb2302cacc1d
htwm623 <htwm623@outlook.com>
2021-06-26 16:48:16 +0800

    添加get示例

---
+++ server.js
@@ -0,0 +1,19 @@
+const fastify = require('fastify')({ logger: true });
+const PORT = 5000;
+
+const items = require('./Items')
+
+fastify.get('/item', (req, reply) => {
+  reply.send(items)
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT);
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
