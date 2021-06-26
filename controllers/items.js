5234347f1d66a969598adc283f1c1e67a3d70f00
htwm623 <htwm623@outlook.com>
2021-06-26 18:03:06 +0800

    上传fastify-api course

--- controllers/items.js
+++ controllers/items.js
@@ -1,17 +1,44 @@
-const items = require('../Items');
-
+const { v4: uuidv4 } = require('uuid');
+let items = require('../Items');
 
 const getItems = (req, reply) => {
-  reply.send(items)
-}
+  reply.send(items);
+};
 
 const getItem = (req, reply) => {
   const { id } = req.params;
-    const item = items.find((item) => item.id === id);
-    reply.send(item);
-}
+  const item = items.find((item) => item.id === id);
+  reply.send(item);
+};
+
+const addItem = (req, reply) => {
+  const { name } = req.body;
+  const item = {
+    id: uuidv4(),
+    name,
+  };
+  console.log(item);
+  items = [...items, item];
+  reply.code(201).send(item);
+};
+
+const deleteItem = (req, reply) => {
+  const { id } = req.params;
+  items = items.filter((e) => e.id !== id);
+  reply.send({ message: `Item ${id} has been removed` });
+};
+const updateItem = (req, reply) => {
+  const { id } = req.params;
+  const {name} = req.body
+  items = items.map(item => (item.id === id ? {id, name}: item))
+  item = items.find(item => item.id === id)
+  reply.send(item)
+};
 
 module.exports = {
   getItems,
-  getItem
-}
+  getItem,
+  addItem,
+  deleteItem,
+  updateItem
+};
--- routes/items.js
+++ routes/items.js
@@ -1,39 +1,89 @@
-const {getItems, getItem } = require('../controllers/items')
-
+const { getItems, getItem, addItem,deleteItem,updateItem } = require('../controllers/items');
 
 const Item = {
   type: 'object',
   properties: {
-    id: { type: 'integer' },
+    id: { type: 'string' },
     name: { type: 'string' },
   },
-}
+};
 
 const getItemsOpts = {
   schema: {
     response: {
       200: {
         type: 'array',
-        items: Item
+        items: Item,
       },
-    }, 
+    },
   },
-  handler: getItems
+  handler: getItems,
 };
 
 const getItemOpts = {
   schema: {
     response: {
-      200: Item
-    }, 
+      200: Item,
+    },
   },
   handler: getItem,
 };
 
+const postItemOpts = {
+  schema: {
+    body: {
+      type: 'object',
+      required: ['name'],
+      properties: {
+        name: { type: 'string' },
+      },
+    },
+    response: {
+      201: Item,
+    },
+  },
+  handler: addItem,
+}
+const deleteItemOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          message: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  },
+  handler: deleteItem
+}
+
+const updateItemsOpts = {
+  schema: {
+    response: {
+      200: Item,
+    },
+  },
+  handler: updateItem,
+}
 
 function itemRoutes(fastify, options, done) {
+  // get all items
   fastify.get('/items', getItemsOpts);
+
+  // get single item
   fastify.get('/items/:id', getItemOpts);
+
+  // add item
+  fastify.post('/items', postItemOpts);
+
+  // delete item
+  fastify.delete('/items/:id', deleteItemOpts);
+
+  // update item
+  fastify.put('/items/:id', updateItemsOpts)
   done();
 }
 
